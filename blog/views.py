7f6706fee3158cfa79bf3b69e39cd174f974ae69
Mohammad Javad Zare <mjz589.2018@gmail.com>
2023-03-24 00:37:24 +0330

    test deleted and data has been gotten from views
    added a context in blog_single_view in blog app and then using context in blog-single.html (template).

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,5 @@ def blog_view(request):
     return render(request, 'blog/blog-home.html')
 
 def blog_single_view(request):
-    return render(request, 'blog/blog-single.html')
+    context = {'author':'Mikey', 'title':'Mikey has finished his first django project' , 'content':'Hello World! I just finished my first django project successfully.\nBy the way, my real name is Mohmamad Javad Zare, but you can call me Mikey!\nThat\'s all.'}
+    return render(request, 'blog/blog-single.html', context)
