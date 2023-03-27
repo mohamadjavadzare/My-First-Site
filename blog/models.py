c845f3aa139179c9dfab03ad9d938469dcfd153d
Mohammad Javad Zare <mjz589.2018@gmail.com>
2023-03-27 22:04:44 +0330

    create Contact in website.models and migrate it

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    # image
+    # author
     title = models.CharField(max_length=100)
     content = models.TextField()
-
-
+    # category
+    # tags
+    counted_views = models.PositiveIntegerField(default=0)
+    publish_status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
